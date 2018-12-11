c99f077116dd6ae7a0b2d257c6b67895c33b82f2
Alex Root <newrou@mail.ru>
2018-12-11 09:11:23 +0700

    Make deconvolution for 1a,1d DS

--- R-stat/MakeAllDat.sh
+++ R-stat/MakeAllDat.sh
@@ -3,6 +3,6 @@
 for i in *.csv; do
  Name=`basename "$i" .csv`
  echo $Name
- ./sect <$i | grep -v "NaN" > $Name.dat
+ ./sect <$i | grep -v "NaN" | sed -e "s/Normalized\ heat\ flow/Normalized_heat_flow/"> $Name.dat
  ./start-R.sh DeconvolutionKinetic.R $Name
 done
